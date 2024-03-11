00c2e60d54f3c33619f1b8878e1f105939434fba
Thomas Heigl <thnew2@gmail.com>
2024-03-11 16:11:04 +0000

    Updated the nutrition dataset with the complete dataset

--- app/classes/get_nutrition_values.py
+++ app/classes/get_nutrition_values.py
@@ -2,8 +2,7 @@ import pandas as pd
 import difflib
 from difflib import SequenceMatcher
 
-# TODO: Replace with real dataset
-nutrition_dataset = pd.read_csv('data/test_nutrition_dataset.csv', delimiter=';')
+nutrition_dataset = pd.read_csv('data/nutrition_dataset.csv', delimiter=',')
 
 food_names = nutrition_dataset['Meal'].dropna().tolist()
 
