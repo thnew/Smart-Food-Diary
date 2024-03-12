c984e34a88956e82c7b23c65849bd55f7112e884
Thomas Heigl <thnew2@gmail.com>
2024-03-12 09:39:01 +0000

    Made page independent from dark mode

--- app/app.py
+++ app/app.py
@@ -5,9 +5,22 @@ from classes.meal_input import MealInput
 from annotated_text import annotated_text
 
 title_cell_1, title_cell_2 = st.columns([3, 1])
+# title_cell_1.markdown("""
+#     <h1>Food Diary</h1>
+#     <style>
+#         h1 span {
+#             font-size: 72px;
+#             background: -webkit-linear-gradient(45deg, #9883E5, #72A1E5 80%);
+#             -webkit-background-clip: text;
+#             -webkit-text-fill-color: transparent;
+#         }
+#     </style>
+#     """, unsafe_allow_html=True)
 title_cell_1.title("Food Diary")
 st.markdown("Start filling out your diary to see your calories")
 
+# title_cell_2.text('')
+title_cell_2.text('')
 title_cell_2.text('')
 config_popover = title_cell_2.popover("Config")
 use_chat_gpt = config_popover.toggle('Use ChatGPT for meal extraction')
