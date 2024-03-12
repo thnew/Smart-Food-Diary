6888505b7ad11a345c2a290582d5f1a90199b351
Thomas Heigl <thnew2@gmail.com>
2024-03-12 22:49:58 +0000

    Cleaned up custom textarea and added a spinner

--- app/components/highlighted_textarea/frontend/src/index.tsx
+++ app/components/highlighted_textarea/frontend/src/index.tsx
@@ -1,9 +1,104 @@
 import { Streamlit, RenderData } from "streamlit-component-lib"
 
-function getLabeledText(
-  text: string,
-  labels: [start: number, end: number, color: string][]
-): Node[] {
+const container = document.querySelector(".textarea-container") as HTMLElement
+const textarea = document.querySelector(".edit-content") as HTMLTextAreaElement
+
+textarea.addEventListener("focus", () => container.classList.add("focused"))
+textarea.addEventListener("blur", () => container.classList.remove("focused"))
+
+let value: string = ""
+textarea.addEventListener("blur", () => {
+  value = value
+    .split("\n")
+    .map((x: string) => x.trim())
+    .join("\n")
+  const inputValue = textarea.innerText
+    .split("\n")
+    .map((x: string) => x.trim())
+    .join("\n")
+
+  const valueChanged = value !== inputValue
+  if (!valueChanged) return
+
+  container.querySelectorAll(".edit-content-back").forEach((el) => el.remove())
+
+  console.log(value)
+  console.log(inputValue)
+
+  Streamlit.setComponentValue(textarea.innerText)
+  Streamlit.setFrameHeight()
+})
+
+textarea.oninput = () => {
+  container.querySelectorAll(".edit-content-back").forEach((el) => el.remove())
+}
+
+/**
+ * The component's render function. This will be called immediately after
+ * the component is initially loaded, and then again every time the
+ * component gets new data from Python.
+ */
+Streamlit.events.addEventListener(
+  Streamlit.RENDER_EVENT,
+  async (event: Event) => {
+    // Get the RenderData from the event
+    const data = (event as CustomEvent<RenderData>).detail
+
+    console.log("Data received in the frontend: ", data)
+
+    // RenderData.args is the JSON dictionary of arguments sent from the
+    // Python script.
+    value = data.args["value"]
+    textarea.innerText = value
+
+    // We tell Streamlit to update our frameHeight after each render event, in
+    // case it has changed. (This isn't strictly necessary for the example
+    // because our height stays fixed, but this is a low-cost function, so
+    // there's no harm in doing it redundantly.)
+    Streamlit.setFrameHeight()
+
+    let labels = await determineLabels(data)
+    refreshLabels(labels)
+  }
+)
+
+// Tell Streamlit we're ready to start receiving data. We won't get our
+// first RENDER_EVENT until we call this function.
+Streamlit.setComponentReady()
+
+// Finally, tell Streamlit to update our initial height. We omit the
+// `height` parameter here to have it default to our scrollHeight.
+Streamlit.setFrameHeight()
+
+type LabelDefinition = [start: number, end: number, color: string]
+
+async function determineLabels(data: RenderData) {
+  showSpinner()
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+  hideSpinner()
+
+  return data.args["labels"] as [start: number, end: number, color: string][]
+}
+
+function showSpinner() {
+  const spinner = document.querySelector(".edit-content-spinner") as HTMLElement
+  spinner.style.display = "block"
+}
+
+function hideSpinner() {
+  const spinner = document.querySelector(".edit-content-spinner") as HTMLElement
+  spinner.style.display = "none"
+}
+
+// Some functions
+function refreshLabels(labels: LabelDefinition[]) {
+  container.querySelectorAll(".edit-content-back").forEach((el) => el.remove())
+  getLabeledText(textarea.innerText, labels).forEach((el) =>
+    container.prepend(el)
+  )
+}
+
+function getLabeledText(text: string, labels: LabelDefinition[]): Node[] {
   const elements: Node[] = []
 
   labels.forEach(([start, end, color]) => {
@@ -32,86 +127,3 @@ function getLabeledText(
 
   return elements
 }
-
-// Add text and a button to the DOM. (You could also add these directly
-// to index.html.)
-const container = document.body.appendChild(document.createElement("div"))
-container.classList.add("textarea-container")
-
-const textarea = container.appendChild(document.createElement("div"))
-textarea.classList.add("edit-content")
-textarea.setAttribute("contenteditable", "true")
-
-/**
- * The component's render function. This will be called immediately after
- * the component is initially loaded, and then again every time the
- * component gets new data from Python.
- */
-Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, (event: Event) => {
-  // Get the RenderData from the event
-  const data = (event as CustomEvent<RenderData>).detail
-
-  console.log("Data received in the frontend: ", data)
-
-  textarea.onfocus = () => {
-    container.classList.add("focused")
-  }
-
-  textarea.onblur = () => {
-    container.classList.remove("focused")
-
-    value = value
-      .split("\n")
-      .map((x: string) => x.trim())
-      .join("\n")
-    const inputValue = textarea.innerText
-      .split("\n")
-      .map((x: string) => x.trim())
-      .join("\n")
-
-    if (value === inputValue) return
-
-    container
-      .querySelectorAll(".edit-content-back")
-      .forEach((el) => el.remove())
-
-    console.log(value)
-    console.log(inputValue)
-
-    Streamlit.setComponentValue(textarea.innerText)
-    Streamlit.setFrameHeight()
-  }
-
-  // RenderData.args is the JSON dictionary of arguments sent from the
-  // Python script.
-  let value = data.args["value"]
-  let labels = data.args["labels"]
-
-  // Show "Hello, name!" with a non-breaking space afterwards.
-  textarea.oninput = () => {
-    container
-      .querySelectorAll(".edit-content-back")
-      //.forEach((el) => el.remove())
-      .forEach((el) => ((el as HTMLElement).style.opacity = "0"))
-  }
-  textarea.innerText = value
-
-  container.querySelectorAll(".edit-content-back").forEach((el) => el.remove())
-  getLabeledText(textarea.innerText, labels).forEach((el) =>
-    container.prepend(el)
-  )
-
-  // We tell Streamlit to update our frameHeight after each render event, in
-  // case it has changed. (This isn't strictly necessary for the example
-  // because our height stays fixed, but this is a low-cost function, so
-  // there's no harm in doing it redundantly.)
-  Streamlit.setFrameHeight()
-})
-
-// Tell Streamlit we're ready to start receiving data. We won't get our
-// first RENDER_EVENT until we call this function.
-Streamlit.setComponentReady()
-
-// Finally, tell Streamlit to update our initial height. We omit the
-// `height` parameter here to have it default to our scrollHeight.
-Streamlit.setFrameHeight()
